05dfd5be4c170f68bd9a344485bb8e736134da3d
theo mallesson <tmalless@inception.42.fr>
2024-03-23 23:09:10 +0100

    nginx check

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -1,54 +1,16 @@
-#!/bin/sh
-
-#replace by healthcheck on compose file
-# sleep 10
-
-# # check if wp-config.php exist
-# if [ -f ./wp-config.php ]
-# then
-# 	echo "wordpress already downloaded"
-# else
-#     # wp core download
-# 	#Download wordpress and all config file
-# 	wget http://wordpress.org/latest.tar.gz
-# 	tar xfz latest.tar.gz
-# 	mv wordpress/* .
-# 	rm -rf latest.tar.gz
-# 	rm -rf wordpress
-
-# 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-# 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-# 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-# 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-# 	cp wp-config-sample.php wp-config.php
-
-# fi
-
-# # if [ ! -f "/var/www/html/index.html" ]; then
-# #     wp core download
-# #     wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci"
-# #     wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
-# #     wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD
-# #     wp theme install inspiro --activate
-# #     wp plugin update --all 
-# # fi
-
-# exec "$@"
-# # php-fpm7 --nodaemonize
-
 #!/bin/bash
 #set -eux
 
 cd /var/www/html/wordpress
 
 if ! wp core is-installed --allow-root; then
-echo "1"
+
 wp config create	--allow-root --dbname=${MYSQL_DATABASE} \
 			--dbuser=${MYSQL_USER} \
 			--dbpass=${MYSQL_PASSWORD} \
 			--dbhost=${MYSQL_HOSTNAME} \
 			--url=https://${DOMAIN_NAME};
-echo "2"
+
 wp core install	--allow-root \
 			--url=https://${DOMAIN_NAME} \
 			--title=${WP_TITLE} \
@@ -66,6 +28,8 @@ wp cache flush --allow-root
 # it provides an easy-to-use interface for creating custom contact forms and managing submissions, as well as supporting various anti-spam techniques
 echo "5"
 wp plugin install contact-form-7 --activate --allow-root
+
+wp plugin install classic-editor --activate --allow-root
 
 # set the site language to English
 echo "6"
